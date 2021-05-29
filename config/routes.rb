d8e738b8c5dec6c3324763eceeeb64bdb8a7da80
Ankit Sharma <ankitsharma.1191@gmail.com>
2021-05-30 01:05:59 +0530

    0.1.7

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :course
+  resources :course do
+     member do
+	get :delete
+     end
+  end
 end
